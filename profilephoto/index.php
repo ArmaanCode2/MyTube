b6c6edc42bc3dcdb0d4c4c462c79bfcc52574c96
ArmaanCode2 <armaansecond9@gmail.com>
2022-05-28 13:32:52 +0530

    made alerts for errors

--- profilephoto/index.php
+++ profilephoto/index.php
@@ -20,17 +20,38 @@ if(isset($_POST['upload'])){
                 $_SESSION['pastdedstination'] = $fileDestination;
                 move_uploaded_file($fileTmpName,$fileDestination);
                 $fullDestination = "profilephoto/" . $fileDestination;
-                $sql = "UPDATE `register` SET `profile_url` = '$fullDestination' WHERE `id` = $id;";
+                $sql = "UPDATE `register` SET `profile_url` = '$fullDestination' WHERE `id` = $id";
                 $_SESSION['photo'] = $fullDestination;
                 $result = mysqli_query($conn,$sql);
             }else{
-                echo 'File is too big';
+                echo '<div style="z-index:2;position:absolute;width:100%;top:0;" class="alert alert-warning d-flex alert-dismissible align-items-center fade show" role="alert">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                            </svg>
+                            <div> Error. File is Too big 
+                            </div>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                     </div>';
             }
         }else{
-            echo 'An Unknown  Error occured Please try Again';
-        }
-    }else{
-        echo 'not supported';
+            echo '<div style="z-index:2;position:absolute;width:100%;top:0;" class="alert alert-warning d-flex alert-dismissible align-items-center fade show" role="alert">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                            </svg>
+                            <div> Error. An Unknown  Error occured Please try Again
+                            </div>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>';
+                }
+            }else{
+        echo '<div style="z-index:2;position:absolute;width:100%;top:0;" class="alert alert-warning d-flex alert-dismissible align-items-center fade show" role="alert">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                        </svg>
+                        <div> Error. File Not Supported. Try using a JPG or PNG file
+                        </div>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>';
     }
 }
 ?>
@@ -43,6 +64,12 @@ if(isset($_POST['upload'])){
     <title>Upload video</title>
     <link rel="stylesheet" href="style.css?version=3">
     <script src="https://cdn.lordicon.com/lusqsztk.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style.css?version=1">
 </head>
 <body>
         <div class="upload">
